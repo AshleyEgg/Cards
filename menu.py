dda81c4898e44e7576c9d53d735ead4866517da9
Ashley <39559276+AshleyEgg@users.noreply.github.com>
2021-04-13 21:38:33 -0700

    Added game select option to menu

--- menu.py
+++ menu.py
@@ -8,21 +8,18 @@ surface = pygame.display.set_mode((600, 400))
 # TODO: Adapt menu to show game types
 
 
-def set_difficulty(value, difficulty):
-    # Do the job here !
-    pass
-
-
 def start_the_game():
     # Do the job here !
     pass
 
 
-menu = pygame_menu.Menu(300, 400, 'Welcome',
-                       theme=pygame_menu.themes.THEME_BLUE)
+menu = pygame_menu.Menu(400, 500, 'Welcome', theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.text_input('Name :', default='John Doe')
-menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.text_input('Name :', default='Ashley')
+menu.add.dropselect(
+    title='Select Game',
+    items=[('GoFish', 1), ('BlackJack', 2), ('BlackJack Training', 3)]
+)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
